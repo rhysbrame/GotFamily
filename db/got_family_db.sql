acae3f1d2b7a736fa0f6dd60eacedeeddd9552ee
rhysbrame <rhysbrame@gmail.com>
2017-09-11 14:25:27 +0100

    added an sql runner and a sql db file

---
+++ db/got_family_db.sql
@@ -0,0 +1,17 @@
+DROP TABLE characters;
+DROP TABLE houses;
+
+CREATE TABLE houses(
+  id SERIAL8 primary key,
+  name VARCHAR(255) NOT NULL
+);
+
+CREATE TABLE characters(
+  id SERIAL8 primary key,
+  fName VARCHAR(255) NOT NULL,
+  sName VARCHAR(255) NOT NULL,
+  gender CHAR(1) NOT NULL,
+  house_id INT8 REFERENCES houses(id),
+  mother_id INT8 REFERENCES characters(id) NULL,
+  father_id INT8 REFERENCES characters(id) NULL
+);
